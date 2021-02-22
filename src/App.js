83feda1ba2754d218195b10eca40a9fa15ec66e8
Alfian Busyro <arufian703@gmail.com>
2021-02-22 14:56:59 +0900

    add capability to calculate screen size

--- src/App.js
+++ src/App.js
@@ -15,8 +15,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         Your screen:<br />
-        Width: {screenWidth}<br />
-        Height: {screenHeight}<br />
+        Width: {screenWidth} pixel<br />
+        Height: {screenHeight} pixel<br />
         {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
