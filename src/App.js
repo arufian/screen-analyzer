93e8d6d9feb992f4cae1f575bda8dc8dda9b5fee
Alfian Busyro <arufian703@gmail.com>
2021-05-10 15:03:24 +0900

    update title and remove unnecesssary thing

--- src/App.js
+++ src/App.js
@@ -28,18 +28,6 @@ function App() {
         Your screen:<br />
         Width: {screenWidth} pixel<br />
         Height: {screenHeight} pixel<br />
-        {/* <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
       </header>
     </div>
   );
